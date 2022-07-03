aa02b2bf37e1162e6961fd1aeeb12cd66ba91e62
amrutakatkar <99672543+amrutakatkar@users.noreply.github.com>
2022-07-03 19:54:02 +0530

    DomainUserDetails.java

---
+++ DomainUserDetails.java
@@ -0,0 +1,62 @@
+package com.example.demo.model;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class DomainUserDetails implements UserDetails{
+	
+	private final User user;
+	
+
+	public DomainUserDetails(User user) {
+		
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return this.user.getRoles().stream().map(role -> role.getRoleName()).map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toSet());
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.user.getName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
